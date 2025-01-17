5f5023116d2c1fc2116c1bce60b88a6b43632318
lijiahao <lijiahao@cmschina.com.cn>
2025-01-17 18:05:02 +0800

    update

--- src/views/Home/components/Download.vue
+++ src/views/Home/components/Download.vue
@@ -16,7 +16,7 @@
       </div>
       <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4 justify-center">
         <a
-          href="https://github.com/pangxiaobin/image-matting"
+          href="https://github.com/Geocld/PeaSyo/releases"
           target="_blank"
           class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
         >
